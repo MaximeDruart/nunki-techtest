d84c375c0e810c38582b2749b24a84504b3f0a49
Maxime Druart <mdruart@area17.com>
2021-08-25 19:36:51 +0200

    added swagger api docs

--- routes/twitter.js
+++ routes/twitter.js
@@ -7,19 +7,59 @@ const T = new Twit({
   app_only_auth: true,
 })
 
-// @route GET /search?q=searchkeyword
-// @desc searches content related to the keyword on twitter
-// @access Public
+/**
+ * @swagger
+ * /search?q=keyword:
+ *   get:
+ *     summary: Get a list of tweets related to keyword
+ *     tags: [tweets]
+ *     produces:
+ *       - application/json
+ *     description : Sends back a list of tweets related to keyword. If no tweets are found an  empty array will be sent
+ *     parameters:
+ *       - in: path
+ *         name: keyword
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Keyword
+ *       - in: path
+ *         name: count
+ *         schema:
+ *           type: string
+ *         description: (optional) the number of items you want to receive (defaults to 10)
+ *     responses:
+ *       200:
+ *         description: The list of tweets
+ */
+
 router.get("/search", (req, res) => {
-  T.get("search/tweets", { q: req.query.q, count: 10 }, (error, data, response) => {
+  T.get("search/tweets", { q: req.query.q, count: req.query.count || 10 }, (error, data, response) => {
     if (error) return res.status(error.statusCode || 500).json({ error })
     return res.status(200).json({ tweets: data.statuses })
   })
 })
 
-// @route GET /users/:id
-// @desc gets detail of a user by id
-// @access Public
+/**
+ * @swagger
+ * /users/:id:
+ *   get:
+ *     summary: Get the user by id
+ *     tags: [users]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The user id
+ *     responses:
+ *       200:
+ *         description: The user description by id
+ *       404:
+ *         description: No user matches for specified terms.
+ */
+
 router.get("/users/:id", (req, res) => {
   T.get(`users/lookup`, { user_id: req.params.id }, (error, data, response) => {
     if (error) return res.status(error.statusCode).json({ error })
--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 const express = require("express")
 const cors = require("cors")
 const app = express()
+const swaggerUI = require("swagger-ui-express")
+const swaggerJsDoc = require("swagger-jsdoc")
 require("dotenv").config()
 
 const PORT = process.env.PORT || 5001
 
 app.use(cors())
 app.use(express.json())
+
+const specs = swaggerJsDoc({
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Twitter API",
+      version: "1.0.0",
+      description: "Express twitter API",
+    },
+    servers: [
+      {
+        url: `http://localhost:${PORT}`,
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+})
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs))
 
 const twitterRouter = require("./routes/twitter")
 app.use("/", twitterRouter)
